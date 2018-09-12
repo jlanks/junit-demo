8ddd0c2753deb9245585daea262852c85d972711
Julian Lankstead <Julian@dhcp-134-159.cu-wireless-students.carleton.ca>
2018-09-12 13:37:11 -0400

    main: added ArithmeticSolver class implementation

---
+++ src/main/java/core/ArithmeticSolver.java
@@ -0,0 +1,29 @@
+package core;
+
+public class ArithmeticSolver {
+	
+	public double add(double x, double y) {
+		return x + y; 
+		
+	}
+	
+	public double subtract(double x, double y) {
+		return x - y; 
+		
+	}
+	
+	public double multiply(double x, double y) {
+		return x * y; 
+		
+	}
+	
+	public double divide(double x, double y) {
+		return x / y; 
+		
+	}
+	public double modulus(double x, double y) {
+		return x % y; 
+		
+	}
+
+}
--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -7,31 +7,31 @@ public class ArithmeticSolverTest extends TestCase {
 	public void testAdd() {
 		
 		ArithmeticSolver solver = new ArithmeticSolver(); 
-		assertEquals(4,solver.add(2,2)); 
+		assertEquals(4.0,solver.add(2,2)); 
 	}
 	
 	
 	public void testSubtract() {
 		
 		ArithmeticSolver solver = new ArithmeticSolver(); 
-		assertEquals(2,solver.subtract(4,2)); 
+		assertEquals(2.0,solver.subtract(4,2)); 
 	}
 	
 public void testMultiply() {
 		
 		ArithmeticSolver solver = new ArithmeticSolver(); 
-		assertEquals(8,solver.multiply(4,2)); 
+		assertEquals(8.0,solver.multiply(4,2)); 
 	}
 
 public void testDivide() {
 	
 	ArithmeticSolver solver = new ArithmeticSolver(); 
-	assertEquals(4,solver.divide(8,2)); 
+	assertEquals(4.0,solver.divide(8,2)); 
 }
 
 public void testSModulus() {
 	
 	ArithmeticSolver solver = new ArithmeticSolver(); 
-	assertEquals(1,solver.modulus(5,2)); 
+	assertEquals(1.0,solver.modulus(5,2)); 
 }
 }
